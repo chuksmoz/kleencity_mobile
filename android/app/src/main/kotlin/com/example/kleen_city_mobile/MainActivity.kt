a3c257b6225a318edf6020cca511d2a2b6817b2a
Moses <obikamoses@gmail.com>
2020-04-30 15:25:29 +0100

    File structure

---
+++ android/app/src/main/kotlin/com/example/kleen_city_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kleen_city_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:kleen_city_mobile/src/app.dart';
+
+void main() {
+  runApp(App());
+}
+
